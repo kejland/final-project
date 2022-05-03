63b984b832a4385bca94322b57aca66328a71b1a
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-05-03 07:27:05 -0400

    upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let card1 = document.querySelector("#1");
+let card2 = document.querySelector("#2");
+let card3 = document.querySelector("#3");
+let card4 = document.querySelector("#4");
+let card5 = document.querySelector("#5");
+let card6 = document.querySelector("#6");
+let card7 = document.querySelector("#7");
+let card8 = document.querySelector("#8");
+let card9 = document.querySelector("#9");
+let card10 = document.querySelector("#10");
+let card11 = document.querySelector("#11");
+let card12 = document.querySelector("#12");
+
+
